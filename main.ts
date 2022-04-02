2cf4f95d6ce7709651a4c635f0bde7c41d252d8b
rescobar9 <67067784+rescobar9@users.noreply.github.com>
2022-04-02 00:26:59 -0700

    Added more code

--- main.ts
+++ main.ts
@@ -9,8 +9,18 @@ radio.onReceivedString(function (receivedString) {
     } else if (receivedString == "R") {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 120)
-    } else {
+    } else if (receivedString == "S") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    } else if (receivedString == "LEDR") {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    } else if (receivedString == "LEDL") {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    } else if (receivedString == "LEDO") {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        basic.clearScreen()
     }
 })
 radio.setGroup(1)
